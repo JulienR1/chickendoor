311d0ac9c2468c34cb89ce7d4c54e84e7f4ce2be
Julien Rousseau <julrousseau20@gmail.com>
2021-08-05 23:12:43 -0400

    Fixed env variables from webapp

--- webApp/src/App.tsx
+++ webApp/src/App.tsx
@@ -4,9 +4,7 @@ import { io } from "socket.io-client";
 
 function App(): JSX.Element {
 	useEffect(() => {
-		console.log("use effect");
-
-		const socket = io("http://localhost:8080");
+		const socket = io(process.env.SERVER_ENDPOINT as string);
 		socket.emit("clientConnect");
 		socket.on(SocketChannel.NotifyDoorState, (data) => {
 			console.log("Received notification: ", JSON.parse(data));
--- webApp/webpack.config.js
+++ webApp/webpack.config.js
@@ -1,49 +1,64 @@
-const prod = process.env.REACT_APP_NODE_ENV === "production";
+const prod = process.env.NODE_ENV === "production";
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
-module.exports = {
-	mode: prod ? "production" : "development",
-	entry: "./src/index.tsx",
-	output: {
-		path: __dirname + "/dist/",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				resolve: {
-					extensions: [".ts", ".tsx", ".js", ".json"],
-				},
-				use: "ts-loader",
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							importLoaders: 1,
-						},
-					},
-					"postcss-loader",
-					"sass-loader",
-				],
-			},
-		],
-	},
-	devtool: prod ? undefined : "source-map",
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "index.html",
-		}),
-	],
-	resolve: {
-		alias: {
-			"@shared": path.resolve(__dirname, "../shared"),
+module.exports = () => {
+	const envPath = path.join(__dirname, `/.env.${process.env.NODE_ENV}`.trim());
+	const env = dotenv.config({ path: envPath }).parsed;
+
+	const envKeys = Object.keys(env).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(env[next]);
+		return prev;
+	}, {});
+
+	console.log(envKeys);
+
+	return {
+		mode: prod ? "production" : "development",
+		entry: "./src/index.tsx",
+		output: {
+			path: __dirname + "/dist/",
 		},
-	},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx)$/,
+					exclude: /node_modules/,
+					resolve: {
+						extensions: [".ts", ".tsx", ".js", ".json"],
+					},
+					use: "ts-loader",
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						"style-loader",
+						{
+							loader: "css-loader",
+							options: {
+								importLoaders: 1,
+							},
+						},
+						"postcss-loader",
+						"sass-loader",
+					],
+				},
+			],
+		},
+		devtool: prod ? undefined : "source-map",
+		plugins: [
+			new webpack.DefinePlugin(envKeys),
+			new HtmlWebpackPlugin({
+				template: "index.html",
+			}),
+		],
+		resolve: {
+			alias: {
+				"@shared": path.resolve(__dirname, "../shared"),
+			},
+		},
+	};
 };
